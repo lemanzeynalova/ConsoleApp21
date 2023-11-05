b6825e75d4bdbc74d47565953905fc558e3fcd9e
User <User@DESKTOP-Q9QV65T>
2023-11-05 22:01:09 +0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp21
+{ 
+    public class MyList<T>
+    {
+            private T[] items;
+            private int count;
+
+            public MyList()
+            {
+                items = new T[4];
+                count = 0;
+            }
+
+            public int Capacity
+            {
+                get { return items.Length; }
+                set
+                {
+                    if (value < count)
+                        throw new ArgumentException("Tutum cari hesablamadan kicik deyer ala bilmez.");
+                    if (value != items.Length)
+                    {
+                        T[] newItems = new T[value];
+                        Array.Copy(items, newItems, count);
+                        items = newItems;
+                    }
+                }
+            }
+
+            public int Count
+            {
+                get { return count; }
+            }
+
+            public void Add(T item)
+            {
+                if (count == items.Length)
+                {
+                    Capacity *= 2;
+                }
+                items[count] = item;
+                count++;
+            }
+
+            public void Clear()
+            {
+                Array.Clear(items, 0, count);
+                count = 0;
+            }
+
+            public bool Exists(T item)
+            {
+                return Array.IndexOf(items, item, 0, count) >= 0;
+            }
+
+            public bool Remove(T item)
+            {
+                int index = Array.IndexOf(items, item, 0, count);
+                if (index >= 0)
+                {
+                    for (int i = index; i < count - 1; i++)
+                    {
+                        items[i] = items[i + 1];
+                    }
+                    count--;
+                    items[count] = default(T);
+                    return true;
+                }
+                return false;
+            }
+
+            public void Reverse()
+            {
+                Array.Reverse(items, 0, count);
+            }
+
+            public int IndexOf(T item)
+            {
+                return Array.IndexOf(items, item, 0, count);
+            }
+
+            public int LastIndexOf(T item)
+            {
+                return Array.LastIndexOf(items, item, count - 1, count);
+            }
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp21
+{
+   public class Program
+   {
+        public static void Main()
+        {
+            MyList<int> myList = new MyList<int>();
+
+            Console.WriteLine($"Capacity: {myList.Capacity}");
+            Console.WriteLine($"Count: {myList.Count}");
+
+            myList.Add(1);
+            myList.Add(2);
+            myList.Add(3);
+            myList.Add(4);
+            myList.Add(5);
+
+            Console.WriteLine("Elements in the list:");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.WriteLine($"Capacity: {myList.Capacity}");
+            Console.WriteLine($"Count: {myList.Count}");
+
+            Console.WriteLine($"Is 3 in the list? {myList.Exists(3)}");
+
+            myList.Remove(3);
+
+            Console.WriteLine($"Is 3 in the list after removal? {myList.Exists(3)}");
+
+            Console.WriteLine("Reversing the list...");
+            myList.Reverse();
+
+            Console.WriteLine("Elements in the reversed list:");
+            for (int i = 0; i < myList.Count; i++)
+            {
+                Console.WriteLine(myList[i]);
+            }
+
+            Console.WriteLine($"Index of 2: {myList.IndexOf(2)}");
+            Console.WriteLine($"Last index of 2: {myList.LastIndexOf(2)}");
+
+            myList.Clear();
+
+            Console.WriteLine("Cleared the list. Count: " + myList.Count);
+        }
+    }
+}
+
+
+
+
+
